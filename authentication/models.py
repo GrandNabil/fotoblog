0e31865a5382976a6a6b5db2101732bc2dc5e8a9
Grand Nabil <akambilawani@gmail.com>
2023-10-01 22:45:36 +0100

    a bug fix in choices

--- authentication/models.py
+++ authentication/models.py
@@ -7,8 +7,9 @@ class User(AbstractUser):
     SUSCRIBER = 'SUSCRIBER'
 
     ROLE_CHOICES = (
-        (CREATOR, 'Créateur'), (SUSCRIBER, 'Abonné'),
+        (CREATOR, 'Créateur'),
+        (SUSCRIBER, 'Abonné'),
                     )
     profile_photo = models.ImageField(verbose_name='Photo de profil')
-    role = models.CharField(max_length=30, choices='ROLE_CHOICES', verbose_name='Rôle')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
 
