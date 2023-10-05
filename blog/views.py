eccf111bb3f2bb983f2eb2e08ffe7b157b72c9e5
Grand Nabil <akambilawani@gmail.com>
2023-10-05 12:05:53 +0100

    login required

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required #  restreindre l’accès à des pages pour que seuls les utilisateurs connectés puissent y accéder
 
+@login_required
 def home(request):
     return render(request, 'blog/home.html')
 
